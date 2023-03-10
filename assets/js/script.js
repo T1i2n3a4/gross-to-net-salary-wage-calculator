c71049749317fc471641c1b70b3c656450a6e644
Cristina Stati <cristinastati1990@gmail.com>
2023-03-10 18:02:30 +0000

    Add structure to calculate annual gross income function

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,21 +12,48 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 })
 
+let detailsForm = document.getElementById('details-form')
+form.addEventListener('submit', handleSubmit);
 
-function calculateAnnualGrossWage() {
-
+function handleSubmit(event) {
+    event.preventDefault();
+    
 }
 
+function calculateAnnualGrossWage() {
+    let annualGrossWage;
+    let yourWageInput = document.getElementById("your-wage-input").value;
+    let periodSelected = document.getElementById("period-your").value;
+        if (periodSelected === "year") {
+        let annualGrossWage = yourWageInput;
+        } else if (periodSelected === "month") {
+            let annualGrossWage = yourWageInput * 12;
+        } else if (periodSelected === "fortnight") {
+            let annualGrossWage = yourWageInput / 2 * 52;
+        } else if (periodSelected === "week") {
+            let annualGrossWage = yourWageInput * 52;
+        } else if (periodSelected === "day") {
+            let annualGrossWage = yourWageInput * 5 * 52;
+        }
+        else {
+           let annualGrossWage = yourWageInput * workingHoursInput * 52;
+        }
+        return annualGrossWage;
+    }
+// document.getElementById("gross-wage-result").innerHTML = annualGrossWage;
 function calculateAnnualPaye() {
+    let annualGrossWage = calculateAnnualGrossWage();
+
 
 }
 
 function calculateAnnualUsc() {
-
+    let annualGrossWage = calculateAnnualGrossWage();
 } 
 
 function calculateAnnualPrsi() {
-
+    let annualGrossWage = calculateAnnualGrossWage();
+    let annualPrsi = annualGrossWage * 4 / 100;
 }
 
 function calculateAnnualTotalTax() {
