33684b4f953e48fcdac2aa8aa547d25ec051818a
Cristina Stati <cristinastati1990@gmail.com>
2023-03-12 18:34:01 +0000

    Fix higher income tax at rate one function bug

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,14 +10,13 @@ function handleSubmit(event) {
     const userAnnualGrossWage = calculateUserAnnualGrossWage();
     console.log(`User's annual gross wage is ${userAnnualGrossWage}`);
     const spouseAnnualGrossWage = calculateSpouseAnnualGrossWage();
-    console.log(`Spouse's annual gross wage is ${spouseAnnualGrossWage}`)
-    console.log(`Higher income tax at rate1 is ${calcHigherIncomeTaxAtRate1()}`);
+    console.log(`Spouse's annual gross wage is ${spouseAnnualGrossWage}`);
     console.log(`Higher income is ${getHigherIncome()}`);
+    console.log(`Higher income tax at rate1 is ${higherIncomeTaxRate1()}`);
+    console.log(`Lower income tax at rate1 is ${lowerIncomeTaxRate1()}`);
     const prsi = calculateUserAnnualPrsi();
     console.log(`User's annual annual PRSI is ${prsi}`);
-    
-    
-
+    console.log(`Lower income is ${lowerIncome()}`)
 }
 // document.getElementById("gross-wage-result").innerHTML = annualGrossWage;
 // document.getElementById("gross-wage-result").innerHTML = userWageInput * 12;
@@ -30,7 +29,6 @@ function calculateUserAnnualGrossWage() {
 
     let userWageInput = document.getElementById("user-wage-input").value;
     let userPeriodSelected = document.getElementById("user-selected-period").value;
-    
     if (userPeriodSelected === "month") {
         document.getElementById("gross-wage-result").innerHTML = userWageInput * 12;
         return userWageInput * 12;
@@ -42,8 +40,7 @@ function calculateUserAnnualGrossWage() {
         return userWageInput * 5 * 52;
     } else if (userPeriodSelected === "hour") {
         return userWageInput * document.getElementById("user-working-hs-weekly").value * 52;
-    }
-    else {
+    } else {
         return userWageInput;
     }
 }
@@ -73,42 +70,55 @@ function calculateSpouseAnnualGrossWage() {
 
 /**
  * 
- * Calculates what is the higher income between spouses
+ * Gets the higher income between spouses
  */
 function getHigherIncome() {
-    return calculateUserAnnualGrossWage() >= calculateSpouseAnnualGrossWage() ? calculateUserAnnualGrossWage() : calculateSpouseAnnualGrossWage();  
+    return calculateUserAnnualGrossWage() >= calculateSpouseAnnualGrossWage() ? calculateUserAnnualGrossWage() : calculateSpouseAnnualGrossWage();
 }
+
+const taxRate1 = 20 / 100;
+let coupleBand1 = 49000;
+let bandIncrease = 31000;
+const coupleBand2 = 80000;
+let taxBand1 = document.getElementById("first-tax-band").value;
 
 /**
  * Calculates the tax at rate1 for the higher income 
  */
-function calcHigherIncomeTaxAtRate1() {
-    // let userAnnualGrossWage = calculateUserAnnualGrossWage()
-    const taxRate1 = 20 / 100;
-    let coupleBand1 = 49000;
-    let bandIncrease = 31000;
-    const coupleBand2 = coupleBand1 + bandIncrease;
-    let taxBand1 = document.getElementById("first-tax-band").value;
+function higherIncomeTaxRate1() {
+const taxRate1 = 20 / 100;
+let coupleBand1 = 49000;
+let bandIncrease = 31000;
+const coupleBand2 = 80000;
+let taxBand1 = document.getElementById("first-tax-band").value;
     if (taxBand1 >= coupleBand2) {
-        if (getHigherIncome() <= coupleBand1){
+        if (getHigherIncome() <= coupleBand1) {
             return getHigherIncome() * taxRate1;
         } else {
             return coupleBand1 * taxRate1;
         }
-    }
-    else if (getHigherIncome() <= taxBand1) {
+    } else if (getHigherIncome() <= taxBand1) {
         return getHigherIncome() * taxRate1;
-    } else if (getHigherIncome() > taxBand1) {
-        return taxBand1 * taxRate1;
-    }
+    } else {
+        taxBand1 * taxRate1;
+    }   
 }
 
+/**
+ * Gets the lower income 
+ */
+function lowerIncome() {
+    return calculateUserAnnualGrossWage() >= calculateSpouseAnnualGrossWage() ? calculateSpouseAnnualGrossWage() : calculateUserAnnualGrossWage();
+}
+
+function lowerIncomeTaxRate1() {
+    return lowerIncome() <= bandIncrease ? lowerIncome() * taxRate1 : bandIncrease * taxRate1;
+}
 
 function calculateAnnualPaye() {
-    
+
     const taxRate2 = 40 / 100;
-    
-    }
+}
 
 
 function calculateAnnualUsc() {
@@ -120,7 +130,7 @@ function calculateAnnualUsc() {
  * by using the PRSI rate of 4%
  */
 function calculateUserAnnualPrsi() {
- 
+
     const prsiRate = 4;
     return calculateUserAnnualGrossWage() * prsiRate / 100;
 
