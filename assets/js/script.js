3244b0f01daeede10c1f45529c39f716d31ec9a6
Cristina Stati <cristinastati1990@gmail.com>
2023-03-14 08:49:07 +0000

    Fix the issue for wage result by defining global variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,13 +4,20 @@
 let detailsForm = document.getElementById('details-form');
 detailsForm.addEventListener('submit', handleSubmit);
 
+const taxRate1 = 20 / 100;
+const taxRate2 = 40 / 100;
+let coupleBand1 = 49000;
+let bandIncrease = 31000;
+const coupleBand2 = 80000;
+let taxBand1 = document.getElementById("first-tax-band").value;
+let wageResults = [annualNetWage(), userAnnualGrossWage(), annualPaye(), annualUsc(), annualPrsi(), annualTotalTax()];
+
 function handleSubmit(event) {
     console.log("Calculate Runs");
     event.preventDefault();
-    const userAnnualGrossWage = calculateUserAnnualGrossWage();
-    console.log(`User's annual gross wage is ${userAnnualGrossWage}`);
-    const spouseAnnualGrossWage = calculateSpouseAnnualGrossWage();
-    console.log(`Spouse's annual gross wage is ${spouseAnnualGrossWage}`);
+
+    console.log(`User's annual gross wage is ${userAnnualGrossWage()}`);
+    console.log(`Spouse's annual gross wage is ${spouseAnnualGrossWage()}`);
     console.log(`Higher income is ${getHigherIncome()}`);
     console.log(`Lower income is ${lowerIncome()}`);
     console.log(`Higher income tax at rate1 is ${higherIncomeTaxRate1()}`);
@@ -25,6 +32,8 @@ function handleSubmit(event) {
     console.log(`User's annual NET WAGE/SALARY is ${annualNetWage()}`)
     console.log(monthlyWage());
 
+
+    display();
 }
 // document.getElementById("gross-wage-result").innerHTML = annualGrossWage;
 // document.getElementById("gross-wage-result").innerHTML = userWageInput * 12;
@@ -33,7 +42,7 @@ function handleSubmit(event) {
  * Calculates users's annual gross wage 
  * in dependence of which period was selected 
  */
-function calculateUserAnnualGrossWage() {
+function userAnnualGrossWage() {
 
     let userWageInput = document.getElementById("user-wage-input").value;
     let userPeriodSelected = document.getElementById("user-selected-period").value;
@@ -57,7 +66,7 @@ function calculateUserAnnualGrossWage() {
  * Calculates spouse annual gross wage 
  * in dependence of which period was selected 
  */
-function calculateSpouseAnnualGrossWage() {
+function spouseAnnualGrossWage() {
 
     let spouseWageInput = document.getElementById("spouse-wage-input").value;
     let spousePeriodSelected = document.getElementById("spouse-period-selected").value;
@@ -81,15 +90,10 @@ function calculateSpouseAnnualGrossWage() {
  * Gets the higher income between spouses
  */
 function getHigherIncome() {
-    return calculateUserAnnualGrossWage() >= calculateSpouseAnnualGrossWage() ? calculateUserAnnualGrossWage() : calculateSpouseAnnualGrossWage();
+    return userAnnualGrossWage() >= spouseAnnualGrossWage() ? userAnnualGrossWage() : spouseAnnualGrossWage();
 }
 
-const taxRate1 = 20 / 100;
-const taxRate2 = 40 / 100;
-let coupleBand1 = 49000;
-let bandIncrease = 31000;
-const coupleBand2 = 80000;
-let taxBand1 = document.getElementById("first-tax-band").value;
+
 
 /**
  * Calculates the tax at rate1 
@@ -118,7 +122,7 @@ function higherIncomeTaxRate1() {
  * Gets the lower income 
  */
 function lowerIncome() {
-    return calculateUserAnnualGrossWage() >= calculateSpouseAnnualGrossWage() ? calculateSpouseAnnualGrossWage() : calculateUserAnnualGrossWage();
+    return userAnnualGrossWage() >= spouseAnnualGrossWage() ? spouseAnnualGrossWage() : userAnnualGrossWage();
 }
 
 function lowerIncomeTaxRate1() {
@@ -180,26 +184,26 @@ function annualPaye() {
  */
 function annualUsc() {
     let uscRates = {
-        rate1 : 0.5 / 100,
-        rate2 : 2 / 100,
-        rate3 : 4.5 / 100,
-        rate4 : 8 / 100
+        rate1: 0.5 / 100,
+        rate2: 2 / 100,
+        rate3: 4.5 / 100,
+        rate4: 8 / 100
     }
     let uscBands = {
-        band0 : 13000,
-        band1 : 12012,
-        band2 : 10908,
-        band3 : 47124,
+        band0: 13000,
+        band1: 12012,
+        band2: 10908,
+        band3: 47124,
     };
 
-    let band4 = calculateUserAnnualGrossWage() - (uscBands.band1 + uscBands.band2 + uscBands.band3)
+    let band4 = userAnnualGrossWage() - (uscBands.band1 + uscBands.band2 + uscBands.band3)
 
-    if (calculateUserAnnualGrossWage() <= uscBands.band0) {
+    if (userAnnualGrossWage() <= uscBands.band0) {
         return 0;
-    } else if (calculateUserAnnualGrossWage() <= uscBands.band1 + uscBands.band2) {
-        return uscBands.band1 * uscRates.rate1 + (calculateUserAnnualGrossWage() - uscBands.band1) * uscRates.rate2;
-    } else if (calculateUserAnnualGrossWage() <= uscBands.band1 + uscBands.band2 + uscBands.band3) {
-        return uscBands.band1 * uscRates.rate1 + uscBands.band2 * uscRates.rate2 + (calculateUserAnnualGrossWage() - (uscBands.band1 + uscBands.band2)) * uscRates.rate3;
+    } else if (userAnnualGrossWage() <= uscBands.band1 + uscBands.band2) {
+        return uscBands.band1 * uscRates.rate1 + (userAnnualGrossWage() - uscBands.band1) * uscRates.rate2;
+    } else if (userAnnualGrossWage() <= uscBands.band1 + uscBands.band2 + uscBands.band3) {
+        return uscBands.band1 * uscRates.rate1 + uscBands.band2 * uscRates.rate2 + (userAnnualGrossWage() - (uscBands.band1 + uscBands.band2)) * uscRates.rate3;
     } else
         return uscBands.band1 * uscRates.rate1 + uscBands.band2 * uscRates.rate2 + uscBands.band3 * uscRates.rate3 + uscBands.band4 * uscRates.rate4;
 }
@@ -211,7 +215,7 @@ function annualUsc() {
 function annualPrsi() {
 
     const prsiRate = 4;
-    return calculateUserAnnualGrossWage() * prsiRate / 100;
+    return userAnnualGrossWage() * prsiRate / 100;
 }
 
 /**Calculates total annual tax
@@ -222,7 +226,7 @@ function annualTotalTax() {
         annualPaye(),
         annualUsc(),
         annualPrsi(),
-        ];
+    ];
     let sum = 0;
     for (i = 0; i < totalTax.length; i += 1) {
         sum += totalTax[i];
@@ -232,47 +236,49 @@ function annualTotalTax() {
 
 /**Claculates user's annual net wage 
  * reducing annual gross wage by annual total tax
-*/
+ */
 function annualNetWage() {
-return calculateUserAnnualGrossWage() - annualTotalTax();
+    return userAnnualGrossWage() - annualTotalTax();
 }
 
 
-let wageResults = [annualNetWage(), calculateUserAnnualGrossWage(), annualPaye(), annualUsc(), annualPrsi(), annualTotalTax()];
+// let wageResults = [annualNetWage(), userAnnualGrossWage(), annualPaye(), annualUsc(), annualPrsi(), annualTotalTax()];
 
 function monthlyWage() {
+    monthlyWageResults = [];
 
     for (i = 0; i < wageResults.length; i++) {
-    wageResults[i] / 12;
+
+        monthlyWageResults[i] = wageResults[i] / 12;
     }
-    
+    console.log(wageResults);
 }
 
 function display() {
 
-    let annualWageComponents = [annualNetWage(), calculateUserAnnualGrossWage(), annualPaye(), annualUsc(), annualPrsi(), annualTotalTax()];
+
     let resultColumn = document.getElementsByTagName('td');
-    console.log(annualWageComponents);
-    console.log(resultColumn);
-    for (i = 0; i < annualWageComponents.length; i++) {
-        document.getElementsByTagName('td').innerHTML = resultColumn[i];
+    // console.log(annualWageComponents);
+    // console.log(resultColumn);
+    for (i = 0; i < wageResults.length; i++) {
+        resultColumn[i + 1].innerHTML = wageResults[i];
     };
-    
-     }
 
-     document.getElementById("mouse-over").addEventListener("mouseover", mouseOver);
-     document.getElementById("mouse-over").addEventListener("mouseout", mouseOut);
-     
-     function mouseOver() {
-       document.getElementById("mouse-over").style.backgroundColor = "lightgray";
-     }
-     
-     function mouseOut() {
-       document.getElementById("mouse-over").style.backgroundColor= "gray";
-     }
+}
+
+document.getElementById("mouse-over").addEventListener("mouseover", mouseOver);
+document.getElementById("mouse-over").addEventListener("mouseout", mouseOut);
+
+function mouseOver() {
+    document.getElementById("mouse-over").style.backgroundColor = "lightgray";
+}
+
+function mouseOut() {
+    document.getElementById("mouse-over").style.backgroundColor = "gray";
+}
 
 
-     // Get the modal
+// Get the modal
 var modal = document.getElementById("myModal");
 
 // Get the button that opens the modal
@@ -282,18 +288,18 @@ var btn = document.getElementById("myBtn");
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
+btn.onclick = function () {
+    modal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
+span.onclick = function () {
+    modal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
 }
